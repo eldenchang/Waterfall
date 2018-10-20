80dd22354cf80bd270de1894ff10c126a8c01616
Qifan Chang <qchang@Qifans-MacBook.local>
2018-10-20 10:56:16 -0400

    added popup window animation

--- Waterfall/AddTaskViewController.swift
+++ Waterfall/AddTaskViewController.swift
@@ -13,12 +13,38 @@ class AddTaskViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        self.showAnimate()
+        
     }
     
     @IBAction func CloseAddTask(_ sender: Any) {
-        self.view.removeFromSuperview()
+        //self.view.removeFromSuperview()
+        self.removeAnimate()
+    }
+    
+    func showAnimate()
+    {
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate()
+    {
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
     }
     
     /*
